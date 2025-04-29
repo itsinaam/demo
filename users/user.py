8ef4342f7bff251c777f43595aab533a198bb9c3
Inaam Ullah <99404590+itsinaam@users.noreply.github.com>
2025-04-29 12:04:57 +0500

    adding test file

---
+++ users/user.py
@@ -0,0 +1,8 @@
+# Function to reverse a string in Python
+def reverse_string(input_string):
+    return input_string[::-1]
+
+# Test the function
+input_str = 'hello'
+result = reverse_string(input_str)
+print(f'The reversed string is: {result}')
