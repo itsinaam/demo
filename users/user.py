c08a6d84262f37f96cfe8bb8fd75622d54cecc05
Inaam Ullah <99404590+itsinaam@users.noreply.github.com>
2025-04-29 12:42:40 +0500

    Delete users directory

--- users/user.py
+++
@@ -1,8 +0,0 @@
-# Function to reverse a string in Python
-def reverse_string(input_string):
-    return input_string[::-1]
-
-# Test the function
-input_str = 'hello'
-result = reverse_string(input_str)
-print(f'The reversed string is: {result}')
